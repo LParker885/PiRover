68b23b3e7e23e44879e30bd1c670c3b3339cfd07
LParker885 <67603811+LParker885@users.noreply.github.com>
2021-01-05 14:56:29 -0700

    Add files via upload

---
+++ pumparm.py
@@ -0,0 +1,17 @@
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
+import time
+
+
+def arm():
+    kit.servo[11].set_pulse_width_range(1000,2000)
+
+    kit.servo[1].angle = 0
+    time.sleep(2)
+    kit.servo[1].angle = 180
+    time.sleep(2)
+    kit.servo[1].angle = 0
+    time.sleep(2)
+    kit.servo[1].angle = 10
+    time.sleep(2)
+    kit.servo[1].angle = 0
---
+++ pumptest.py
@@ -0,0 +1,53 @@
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
+import time
+
+
+
+
+kit.servo[11].set_pulse_width_range(1100,1500)
+kit.servo[5].set_pulse_width_range(1500,2400)
+kit.servo[4].set_pulse_width_range(1400,2400)
+
+kit.servo[11].angle = 0
+kit.servo[5].angle = 90
+kit.servo[4].angle = 90
+time.sleep(1)
+kit.servo[5].angle = 180
+kit.servo[11].angle = 30
+time.sleep(1)
+kit.servo[5].angle = 90
+kit.servo[11].angle = 0
+time.sleep(1)
+kit.servo[5].angle = 0
+kit.servo[11].angle = 40
+time.sleep(1)
+kit.servo[5].angle = 90
+kit.servo[11].angle = 0
+time.sleep(1)
+kit.servo[4].angle = 180
+kit.servo[11].angle = 30
+time.sleep(1)
+kit.servo[4].angle = 90
+kit.servo[11].angle = 0
+time.sleep(1)
+kit.servo[4].angle = 0
+kit.servo[11].angle = 30
+time.sleep(1)
+kit.servo[4].angle = 90
+kit.servo[11].angle = 0
+time.sleep(1)
+kit.servo[5].angle = 180
+kit.servo[11].angle = 30
+time.sleep(1)
+kit.servo[5].angle = 90
+kit.servo[11].angle = 0
+time.sleep(1)
+kit.servo[5].angle = 0
+kit.servo[11].angle = 40
+time.sleep(1)
+kit.servo[5].angle = 90
+kit.servo[11].angle = 0
+
+kit.servo[5].angle = 90
+kit.servo[4].angle = 90
