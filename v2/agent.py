aaf7129192966acddf1d482a519e162e411fc2d6
LParker885 <67603811+LParker885@users.noreply.github.com>
2023-03-24 11:30:13 -0600

    make changes

---
+++ v2/agent.py
@@ -0,0 +1,25 @@
+
+
+class DwayneJohnson:
+    def __init__(self, topSpeed):
+        #init data array
+        #check motor status
+        #init control mechanism (speed, topspeed, etc)
+
+        pass
+
+    def updateEnv(data):
+        #gather data from inputs, put it into dataarray 
+        pass
+
+    def goFast(left, right):
+        #update control mechanism to match left/right
+        pass
+
+    def drive():
+        #run neuralnet to make prediction of how to gofast
+        #goFast(neuralnetoutleft, neuralnetoutright)
+        #return neuralnet confidence
+        pass
+
+    
